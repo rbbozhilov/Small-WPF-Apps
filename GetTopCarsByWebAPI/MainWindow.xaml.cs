e6973994ee937c560a30f39eb8691a3a7675190b
rbbozhilov <rbbojilov@gmail.com>
2023-11-25 00:34:33 +0200

    Making WPF App for get top cars by web API from my ASP.NET project CarsBg-System

---
+++ GetTopCarsByWebAPI/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+
+using System.Collections.Generic;
+
+using System.Net.Http;
+
+using System.Windows;
+using System.Windows.Controls;
+using TopCars.Models;
+
+namespace TopCars
+{
+
+    public partial class MainWindow : Window
+    {
+
+        private Label name;
+        private Label mark;
+        private Label price;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private async void  GetCars_Click(object sender, RoutedEventArgs e)
+        {
+
+            this.name = new Label();
+            this.name.Content = "Name:";
+
+            this.mark = new Label();
+            this.mark.Content = "Mark:";
+
+            this.price = new Label();
+            this.price.Content = "Price:";
+
+            HttpClient client = new HttpClient();
+            string url = "https://localhost:44353/api/topcars";
+
+           string responseString = await client.GetStringAsync(url);
+
+
+           List<Car> cars =  JsonConvert.DeserializeObject<List<Car>>(responseString);
+
+            int counter = 1;
+            foreach(var car in cars)
+            {
+                Label newLabel = new Label();
+                newLabel.Content = $"{counter} Car";
+                counter++;
+                stackPan.Children.Add(newLabel);
+
+                TextBox carName = new TextBox();
+                carName.Text = car.Name;
+                stackPan.Children.Add(carName);
+
+                TextBox carModel = new TextBox();
+                carModel.Text = car.Mark;
+                stackPan.Children.Add(carModel);
+
+                TextBox carPrice = new TextBox();
+                carPrice.Text = car.Price.ToString();
+                stackPan.Children.Add(carPrice);
+
+
+            }
+
+        }
+    }
+}
---
+++ GetTopCarsByWebAPI/Models/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopCars.Models
+{
+    public class Car
+    {
+
+        public string Name { get; set; }
+
+        public string Mark { get; set; }
+
+        public double Price { get; set; }
+
+    }
+}
